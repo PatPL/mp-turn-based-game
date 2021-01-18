b509a2cdce2413003da3fba8f7084faa5c4764f4
PatPL <patrykwyp@poczta.fm>
2021-01-18 15:26:41 +0100

    Naprawiono błąd powodujący ogromne rozmiary plików wyjściowych

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@ rd /s /q build
 mkdir build
 cd Server/out/production/Server
 jar -cvfe Server.jar GameServer.GameServer *
-jar -cvfe Client.jar Client.ClientGUI *
 move Server.jar ../../../../build/Server.jar
+jar -cvfe Client.jar Client.ClientGUI *
 move Client.jar ../../../../build/Client.jar
 cd ../../../..
 echo "Done"
